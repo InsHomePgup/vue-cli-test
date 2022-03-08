3318b05a2438f9bbecff5849dbcbc866bd5002da
Aka.braham <99671204@qq.com>
2022-03-08 09:18:29 +0800

    vuex getter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,57 @@
 <template>
-  <div class="hello">
-    <div>{{count}}</div>
-    <button @click = "storeClick">click me </button>
-  </div>
+    <div class="hello">
+        <div>{{ count }}</div>
+        <button @click="storeClick">click me</button>
+    </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 export default {
-  computed:mapState(['count']),
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  mounted(){
-    console.log("挂载的store",this.$store.state.count)
-    // this.count = this.$store.state.count
-  },
-  data(){
-    return {
-
-    }
-  },
-  methods:{
-    storeClick(){
-      this.$store.commit('increment')
-    }
-  }
-}
+    computed: {
+        ...mapState({
+            count: "count",
+        }),
+    },
+    name: "HelloWorld",
+    props: {
+        msg: String,
+    },
+    mounted() {
+        this.$store.commit("addObj", {
+            id: "mapbox",
+            list: [1, 2, 3],
+        });
+        // console.log("挂载的store", this.$store.state.count);
+        const list = this.$store.getters.getObjById("mapbox");
+        console.log("获取到的列表", list);
+        // this.count = this.$store.state.count
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        storeClick() {
+            this.$store.commit("increment");
+        },
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
